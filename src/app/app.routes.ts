af57b355415ab2713fbc3f9a0f5673b8a790247f
stefano <stefano.esquerre.1818@gmail.com>
2025-11-04 08:12:42 -0500

    Formulario para insertar un alimento

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import {HomeComponent} from "./componente/home-component/home-component";
 import {AlimentoComponent} from "./componente/alimento-component/alimento-component";
 import {InicioSesion} from './componente/inicio-sesion/inicio-sesion';
 import {RegistrarUsuario} from './componente/registrar-usuario/registrar-usuario';
+import {AlimentoNuevoComponent} from './componente/alimento-nuevo-component/alimento-nuevo-component';
 
 export const routes: Routes = [
   {path: '', component: HomeComponent, pathMatch: 'full'},
   {path: 'Alimento', component: AlimentoComponent},
   {path: 'registro', component: RegistrarUsuario},
-  {path: 'login', component: InicioSesion}
+  {path: 'login', component: InicioSesion},
+  {path: 'NuevoAlimento', component: AlimentoNuevoComponent}
 ];
