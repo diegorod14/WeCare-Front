9ca3c308aa5c73b9f6e3c9186a2cfeb7df34a39f
Diego Rodriguez <diego.rodalv1405@gmail.com>
2025-11-12 23:47:17 -0500

    Desarrollo de login e inicio de sesion incorporados.

---
+++ src/app/interceptor/login-interceptor.ts
@@ -0,0 +1,28 @@
+import {HttpInterceptorFn, HttpStatusCode} from '@angular/common/http';
+import {catchError, EMPTY, throwError} from 'rxjs';
+
+export const loginInterceptor: HttpInterceptorFn = (req, next) => {
+  console.log("Intercepto")
+  const token = localStorage.getItem('token');
+  console.log("Token recuperado:", token);
+  let authReq = req;
+  if (token) {
+    authReq = req.clone({
+      withCredentials : true,
+      headers: req.headers.set('Authorization', 'Bearer '+
+        localStorage.getItem('token')?.toString())
+    });
+    console.log("Se termino de clonar la solicitud con el token.");
+  }
+  return next(authReq).pipe(
+    catchError(error => {
+      console.log("Error en la petición");
+      if (error.status === HttpStatusCode.Forbidden) {
+        alert("NO TIENES PERMISOS!")
+        return EMPTY;
+      } else {
+        return throwError(() => error);
+      }
+    })
+  );
+};
